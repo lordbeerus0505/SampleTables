f4ce5363df49df25437a501287af1c4fa746df6b
Abhiram Natarajan <abhiram.natarajan@gmail.com>
2018-01-20 22:45:41 +0530

    AVG() SQL

---
+++ Average.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+public class Average {
+    public static String Avg(String c)
+    {    /*
+            Finding the average of all elements in the column c_sales
+         */
+        return "AVG("+c+")";
+    }
+    public static void main(String[] args) throws IOException {
+        String s = "average c_price of all products";
+        //String c="c_sales";
+        List<String> l = Files.readAllLines(Paths.get("Average.txt"));
+        List<String> list=new ArrayList<String>();
+        for (String w : s.split(" ")) {
+            list.add(w.toLowerCase());
+
+        }String col1="";
+        String col[]=s.split(" ");
+        for (int i=0;i<col.length;i++)
+        {
+            if(col[i].contains("c_"))
+            {
+                col1=""+col[i];
+                break;
+            }
+        }
+
+        System.out.println(col1);
+        //System.out.println(list);
+
+        for (String t:list)
+        {
+            if(l.containsAll(Collections.singleton(t)))
+            {s=Avg(col1);
+                break;}
+
+        }
+        System.out.println(s);
+
+    }
+}
